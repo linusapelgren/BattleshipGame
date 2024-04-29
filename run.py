be1cec9e1455b0ce024c8595a7a8a03df9ac5bf6
linusapelgren <70014475+linusapelgren@users.noreply.github.com>
2024-04-29 14:41:44 +0200

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+class board1:
+class board2:
+class board3:
+ 
+
+def 
